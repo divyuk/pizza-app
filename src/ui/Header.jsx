b066a2ac83a584ac64f8243eeca19a1f3eb84b96
divyuk <div14kes@gmail.com>
2024-01-31 18:07:59 +0530

    Header Component

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Reactive Pizza 🍕</Link>
+      <p>DK</p>
+    </header>
+  );
+}
+
+export default Header;
