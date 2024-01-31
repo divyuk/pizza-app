e96a5b9622814ffe32bf7d438813c277d54cac18
divyuk <div14kes@gmail.com>
2024-01-31 17:15:32 +0530

    create Browser  Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+]);
+
 function App() {
-  return <div>Helo</div>;
+  return <RouterProvider router={router} />;
 }
 export default App;
