9c52192f85fb2b9d95e283a27f73c975d8d3c44c
divyuk <div14kes@gmail.com>
2024-01-31 18:06:48 +0530

    App Layout for the childrens

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,32 @@ import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/order/:orderId",
-    element: <Order />,
-  },
-  {
-    path: "/order/new",
-    element: <CreateOrder />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+      {
+        path: "/order/new",
+        element: <Order />,
+      },
+    ],
   },
 ]);
 
