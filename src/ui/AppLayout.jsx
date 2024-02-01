2e4868af3ce9b8c3350f977123cd5e04f1b1707d
divyuk <div14kes@gmail.com>
2024-02-01 11:21:20 +0530

    Added navigation of loading state inside app layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
       </main>
       <CartOverview />
-    </>
+    </div>
   );
 }
 
