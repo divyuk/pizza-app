2e591a940c832a9ac032cb82b0c68fda49e8dda0
divyuk <div14kes@gmail.com>
2024-02-01 16:58:22 +0530

    flex and text color onto Header ❄

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Reactive Pizza 🍕</Link>
+    <header className="flex items-center justify-between border-b border-stone-200 bg-purple-200 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-[15px]">
+        Reactive Pizza 🍕
+      </Link>
       <SearchOrder />
-      <p>DK</p>
+      <Username />
     </header>
   );
 }
