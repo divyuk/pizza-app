171966becdad65e9fb121118514e7c100f581230
divyuk <div14kes@gmail.com>
2024-02-01 14:24:46 +0530

    Search Order component inside Header to search 🚕

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Reactive Pizza 🍕</Link>
+      <SearchOrder />
       <p>DK</p>
     </header>
   );
