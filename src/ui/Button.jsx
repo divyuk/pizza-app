5a5dc244458ee6100802a743f90b63345f17932b
divyuk <div14kes@gmail.com>
2024-02-02 20:19:24 +0530

    Added text-sm 🦐

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to, type }) {
   const base =
-    'inline-block rounded-full bg-purple-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-purple-300 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-offset-2 disabled:cursor-not-allowed ';
+    'inline-block text-sm rounded-full bg-purple-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-purple-300 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-offset-2 disabled:cursor-not-allowed ';
 
   const styles = {
     primary: base + ' px-4 py-3 sm:px-6 sm:py-4',
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-sx',
     secondary:
-      'inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed  px-4 py-2.5 sm:px-6 sm:py-4 md:py-3.5',
+      'inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed  px-4 py-2.5 sm:px-6 sm:py-4 md:py-3.5',
   };
   if (to)
     return (
