d35f096c20ea1f9d72f4fc5c4518bc2d82d1335e
divyuk <div14kes@gmail.com>
2024-02-02 14:03:13 +0530

    Added custom styles and reusable components 🔲

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import LinkButton from '../../ui/LinkButton';
+import Button from '../../ui/Button';
 
 const fakeCart = [
   {
@@ -29,14 +31,12 @@ function Cart() {
 
   return (
     <div>
-      <Link to="/menu" className="text-sm text-blue-500 hover:text-blue-600">
-        &larr; Back to menu
-      </Link>
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
       <h2>Your cart, %NAME%</h2>
 
       <div>
-        <Link to="/order/new">Order pizzas</Link>
+        <Button to="/order/new">Order pizzas</Button>
         <button>Clear cart</button>
       </div>
     </div>
