86684281ed2d9caf37001cdbcd7c9eafdef61259
divyuk <div14kes@gmail.com>
2024-02-02 14:02:33 +0530

    Added to for links which require to move back and forth ⬅

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,16 @@
-function Button({ children, disabled }) {
+import { Link } from 'react-router-dom';
+
+function Button({ children, disabled, to }) {
+  const className =
+    'inline-block rounded-full bg-purple-400 px-3 py-4 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-purple-300 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
   return (
-    <button
-      disabled={disabled}
-      className="inline-block rounded-full bg-purple-400 px-3 py-4 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-purple-300 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-    >
+    <button disabled={disabled} className={className}>
       {children}
     </button>
   );
