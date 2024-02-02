42a9a90b46a7d665f9e55b7f5a778d99397142f3
divyuk <div14kes@gmail.com>
2024-02-02 20:19:39 +0530

    Added Design ❕

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -40,33 +40,44 @@ function CreateOrder() {
   const isOrdering = navigation.state === 'submitting';
   const formErrors = useActionData();
   return (
-    <div>
-      <h2>Ready to order? Let &apos;s go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">
+        Ready to order? Let &apos;s go!
+      </h2>
 
       {/* <Form method="POST" action="/order/new"> */}
       {/* This will auto point to nearest route */}
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input className="input" type="text" name="customer" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input className="input grow " type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className="input" type="tel" name="phone" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className=" grow">
+            <input className="input w-full" type="tel" name="phone" required />
           </div>
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input className="input" type="text" name="address" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              className="input w-full"
+              type="text"
+              name="address"
+              required
+            />
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-purple-400 focus:outline-none focus:ring focus:ring-purple-400 focus:ring-offset-2"
             type="checkbox"
@@ -75,7 +86,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
