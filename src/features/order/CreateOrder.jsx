d483546226eb08577173bc897c3c0af54e1d5102
divyuk <div14kes@gmail.com>
2024-02-02 13:45:22 +0530

    Styles applied on these components 💄

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,30 +1,30 @@
-import { createOrder } from "../../services/apiRestaurant";
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from '../../services/apiRestaurant';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -36,37 +36,38 @@ function CreateOrder() {
   const cart = fakeCart;
 
   const navigation = useNavigation();
-  const isOrdering = navigation.state === "submitting";
+  const isOrdering = navigation.state === 'submitting';
   const formErrors = useActionData();
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      <h2>Ready to order? Let &apos;s go!</h2>
 
       {/* <Form method="POST" action="/order/new"> */}
       {/* This will auto point to nearest route */}
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
         </div>
 
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input className="input" type="text" name="address" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
           <input
+            className="h-6 w-6 accent-purple-400 focus:outline-none focus:ring focus:ring-purple-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
@@ -78,8 +79,11 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isOrdering}>
-            {isOrdering ? "Placing order..." : "Order now"}
+          <button
+            disabled={isOrdering}
+            className="inline-block rounded-full bg-purple-400 px-3 py-4 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-purple-300 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
+            {isOrdering ? 'Placing order...' : 'Order now'}
           </button>
         </div>
       </Form>
@@ -94,11 +98,11 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === 'on',
   };
   const errors = {};
   if (!isValidPhone(order.phone))
-    errors.phone = "Please give us your correct phone number";
+    errors.phone = 'Please give us your correct phone number';
   if (Object.keys(errors).length > 0) return errors;
   const newOrder = await createOrder(order);
 
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SearchOrder() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!query) return;
     navigate(`/order/${query}`);
-    setQuery("");
+    setQuery('');
   }
 
   return (
@@ -18,6 +18,7 @@ function SearchOrder() {
         placeholder="Seach Order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-purple-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
