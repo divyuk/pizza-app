0f7a20e5270d2b70902c0e25f4fc52042c717e84
divyuk <div14kes@gmail.com>
2024-02-02 19:02:07 +0530

    added type small and primary in button 🟧

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to }) {
-  const className =
-    'inline-block rounded-full bg-purple-400 px-3 py-4 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-purple-300 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
+function Button({ children, disabled, to, type }) {
+  const base =
+    'inline-block rounded-full bg-purple-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-purple-300 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-offset-2 disabled:cursor-not-allowed ';
+
+  const styles = {
+    primary: base + ' px-4 py-3 sm:px-6 sm:py-4',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-sx',
+  };
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
