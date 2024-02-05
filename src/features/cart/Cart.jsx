5976f178c93d96f990dcb3414852913968f0a6dd
divyuk <div14kes@gmail.com>
2024-02-05 16:57:30 +0530

    Fixed id bug 🐛

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -19,7 +19,7 @@ function Cart() {
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
-          <CartItem item={item} key={item.id} />
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
       <div className="mt-6 space-x-2">
