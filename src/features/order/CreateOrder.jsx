bbab0525bcae087373e6246002f18303236ba8bf
divyuk <div14kes@gmail.com>
2024-02-05 13:41:00 +0530

    Adding defaultValue in the name field 🎢

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { createOrder } from '../../services/apiRestaurant';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -35,6 +36,7 @@ const fakeCart = [
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
+  const username = useSelector((state) => state.user.username);
 
   const navigation = useNavigation();
   const isOrdering = navigation.state === 'submitting';
@@ -50,7 +52,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow " type="text" name="customer" required />
+          <input
+            className="input grow "
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
