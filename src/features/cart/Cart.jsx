4e7f9eef449c57a86d5289aa489c5a3a10e665b5
divyuk <div14kes@gmail.com>
2024-02-05 16:18:50 +0530

    Removing fakeCart data

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -6,30 +6,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart } from './cartSlice';
 import EmptyCart from './EmptyCart';
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function Cart() {
   const cart = useSelector(getCart);
   const username = useSelector((state) => state.user.username);
